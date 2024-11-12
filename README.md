da83b7f39824d8cd0c990ada7850cb727393f30e
Joao Vitor <joaovitormoraveis@gmail.com>
2024-11-11 20:49:07 -0500

    project setup

---
+++ README.md
@@ -0,0 +1,1 @@
+# udemy-gateway-server
---
+++ src/test/java/course/udemy/springboot/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package course.udemy.springboot.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
