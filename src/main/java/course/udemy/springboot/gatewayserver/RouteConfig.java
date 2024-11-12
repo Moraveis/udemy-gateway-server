da1af19a629c2dbf4722c02428f340aa8e64dc1d
Joao Vitor <joaovitormoraveis@gmail.com>
2024-11-11 21:24:47 -0500

    adds custom routes

---
+++ src/main/java/course/udemy/springboot/gatewayserver/RouteConfig.java
@@ -0,0 +1,28 @@
+package course.udemy.springboot.gatewayserver;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RouteConfig {
+
+    @Bean
+    public RouteLocator courseRouterConfig(RouteLocatorBuilder routeLocatorBuilder) {
+        return routeLocatorBuilder.routes()
+                .route(p ->
+                        p.path("/course/accounts/**")
+                                .filters(f -> f.rewritePath("/course/accounts/(?<segment>.*)", "/${segment}"))
+                                .uri("lb://ACCOUNTS"))
+                .route(p ->
+                        p.path("/course/loans/**")
+                                .filters(f -> f.rewritePath("/course/loans/(?<segment>.*)", "/${segment}"))
+                                .uri("lb://LOANS"))
+                .route(p ->
+                        p.path("/course/cards/**")
+                                .filters(f -> f.rewritePath("/course/cards/(?<segment>.*)", "/${segment}"))
+                                .uri("lb://CARDS"))
+                .build();
+    }
+}
