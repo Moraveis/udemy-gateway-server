12208f10e33028221538ba6657b0a08273f2aaf9
Joao Vitor <joaovitormoraveis@gmail.com>
2024-12-18 21:55:56 -0500

    configuring security checks with keycloak

--- src/main/java/course/udemy/springboot/gatewayserver/config/SecurityConfig.java
+++ src/main/java/course/udemy/springboot/gatewayserver/config/SecurityConfig.java
@@ -2,11 +2,16 @@ package course.udemy.springboot.gatewayserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.ReactiveJwtAuthenticationConverterAdapter;
 import org.springframework.security.web.server.SecurityWebFilterChain;
+import reactor.core.publisher.Mono;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -16,11 +21,19 @@ public class SecurityConfig {
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity serverHttpSecurity) {
         return serverHttpSecurity.authorizeExchange(exchanges ->
                         exchanges.pathMatchers(HttpMethod.GET).permitAll()
-                                .pathMatchers("/course/accounts/**").authenticated()
-                                .pathMatchers("/course/cards/**").authenticated()
-                                .pathMatchers("/course/loans/**").authenticated())
-                .oauth2ResourceServer(oAuth2ResourceServerSpec ->
-                        oAuth2ResourceServerSpec.jwt(Customizer.withDefaults()))
+                                .pathMatchers("/course/accounts/**").hasRole("ACCOUNTS")
+                                .pathMatchers("/course/cards/**").hasRole("CARDS")
+                                .pathMatchers("/course/loans/**").hasRole("LOANS"))
+                .oauth2ResourceServer(oAuth2ResourceServerSpec -> oAuth2ResourceServerSpec
+                        .jwt(jwtSpec -> jwtSpec.jwtAuthenticationConverter(grantedAuthoritiesExtractor())))
                 .csrf(ServerHttpSecurity.CsrfSpec::disable).build();
+    }
+
+    private Converter<Jwt, Mono<AbstractAuthenticationToken>> grantedAuthoritiesExtractor() {
+        JwtAuthenticationConverter jwtAuthenticationConverter =
+                new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter
+                (new KeycloakRoleConverter());
+        return new ReactiveJwtAuthenticationConverterAdapter(jwtAuthenticationConverter);
     }
 }
